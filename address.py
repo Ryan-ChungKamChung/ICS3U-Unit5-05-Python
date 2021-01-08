bffd9e4e50b69112e0672657bcc854dfb06d5694
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2021-01-08 20:53:49 +0000

    fixed address.py

--- address.py
+++ address.py
@@ -2,38 +2,39 @@
 
 # Created by Ryan Chung Kam Chung
 # Created in January 2021
-# Program finds volume of a cylinder
+# Program outputs a formatted mailing address
 
 
-def address_format(addressee, street_name, street_number, city_name,
-                   province_name, postal_code, apartment = None):
+def address_format(addressee, street, street_number, city,
+                   province, postal_code, apartment=None):
+    # Formats the inputs from main()
 
+    # Process
     full_address = addressee
-
-    if apartment != None:
+    if apartment is not None:
         full_address = (addressee.upper() + '\n' + apartment + "-"
-                        + street_number + " " + street_name
-                        + '\n' + city_name.upper() + " " +
-                        province_name.upper() + "  " + postal_code.upper())
+                        + street_number + " " + street
+                        + '\n' + city.upper() + " " +
+                        province.upper() + "  " + postal_code.upper())
     else:
         full_address = (addressee.upper() + '\n'
-                        + street_number + " " + street_name
-                        + '\n' + city_name.upper() + " " +
-                        province_name.upper() + "  " + postal_code.upper())
+                        + street_number + " " + street
+                        + '\n' + city.upper() + " " +
+                        province.upper() + "  " + postal_code.upper())
 
-        return full_address
+    return full_address
 
 
 def main():
     # Takes user input, passes it to functions and calls them
-    
+
     apartment = None
 
     print("This tool helps you create a formatted postal address")
 
     # Input
-    addressee = input("Enter Addressee (FIRSTNAME LASTNAME): ")
-    
+    addressee = input("Enter addressee (FIRSTNAME LASTNAME): ")
+
     while True:
         question = input("Do you live in an apartment? (Y/N): ")
         if question.upper() == "Y":
@@ -44,27 +45,29 @@ def main():
         else:
             print("Please enter Y or N")
 
-    street_name = input("Enter street name (street type abbreviated, "
-                        "abbreviated street direction if needed): ")
+    street = input("Enter street name (street type abbreviated): ")
     street_number = input("Enter street number: ")
 
-    city_name = input("Enter city: ")
-    province_name = input("Enter province (Abbreviated): ")
-    postal_code = input("Enter postal code (uppercased, space between the "
-                        "first and last 3 characters): ")
+    city = input("Enter city name: ")
+    province = input("Enter province name (Abbreviated): ")
+    postal_code = input("Enter postal code (first 3 digits separated from"
+                        " the last 3): ")
 
     # Calls functions
-    if apartment != None:
-        address_formatted = address_format(addressee, street_name,
-                                           street_number, city_name,
-                                           province_name, postal_code,
+    if apartment is not None:
+        address_formatted = address_format(addressee, street,
+                                           street_number, city,
+                                           province, postal_code,
                                            apartment)
     else:
-        address_formatted = address_format(addressee, street_name, 
-                                           street_number, city_name,
-                                           province_name, postal_code)
+        address_formatted = address_format(addressee, street,
+                                           street_number, city,
+                                           province, postal_code)
 
+    # Output
+    print(" ")
     print(address_formatted)
+
 
 if __name__ == "__main__":
     main()
